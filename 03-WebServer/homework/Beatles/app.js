545057d0c46382d54db69a873b48ba4bdf515908
Nahuel61920 <estebancarrizo619@gmail.com>
2022-06-30 15:14:30 -0300

    End 03-WebServer

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,60 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+function readFile(filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, "utf-8", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+http.createServer(function(req, res) {
+  if (req.url === '/') {
+    return readFile('./index.html')
+      .then(html => {
+        res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+        res.end(html);
+      })
+      .catch(err => {
+        res.writeHead(500, { "Content-Type": "text/html;charset=UTF-8" });
+        res.end("<h1>Error</h1>");
+      })
+    }
+  
+  if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify(beatles));
+  } else if (req.url.substring(0, 5) === '/api/') {
+    const sites = req.url.substring(5);
+    const beatle = beatles.find(beatle => encodeURI(beatle.name).toLowerCase() === sites.toLowerCase());
+    if (beatle) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify(beatle));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/html;charset=UTF-8' });
+      return res.end('<h1>Not found</h1>');
+    }
+    console.log(beatle);
+  }
+
+  if (req.url[0] === '/' && req.url.length > 1) {
+    const sites = req.url.substring(1);
+    const beatle = beatles.find(beatle => encodeURI(beatle.name).toLowerCase() === sites.toLowerCase());
+    if (beatle) {
+      return readFile('./beatle.html')
+        .then(html => {
+          res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+          res.end(html.replace(/{name}/g, beatle.name).replace('{birthdate}', beatle.birthdate).replace('{profilePic}', beatle.profilePic));
+        })
+    }
+  }
+
+  res.writeHead(404, {'Content-Type': 'text/html;charset=UTF-8'});
+  res.end('<h1>404: Page not found</h1>');
+}).listen(8080, '127.0.0.1');
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,35 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(`./images/${filename}`, (err, data) => {
+            if (err) {
+                reject({
+                    status: 404,
+                    contentType: "text/plain",
+                    data: "<h1>Not found</h1>"
+                });
+            } else {
+                resolve({
+                    data,
+                    contentType: "image/png",
+                    status: 200
+                });
+            }
+        })
+    })
+}
+
+http.createServer(function(req, res) {
+    readFile(req.url.substring(1))
+        .then(data => {
+            res.writeHead(data.status, { "Content-Type": data.contentType });
+            res.end(data.data);
+        })
+        .catch(err => {
+            res.writeHead(err.status, { "Content-Type": err.contentType });
+            res.end(err.data);
+        })
+}).listen(8080, "127.0.0.1")
